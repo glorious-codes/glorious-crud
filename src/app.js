b4f452891480efff7fd8dd5a549e073f4cda4d6f
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-01 23:08:17 -0300

    Added api documentation endpoint and fixed app port in production

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
-const fs = require('fs'),
+const
+  ENV = require('./environment'),
+  fs = require('fs'),
   express = require('express'),
   bodyParser = require('body-parser'),
   cors = require('cors'),
   resources = require('./resources'),
-  port = 9000;
+  port = process.env.NODE_ENV == 'production' ? 80 : 9000;
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 resources.registerAll(app);
+
+app.get('/', (req, res) => {
+  res.send({
+    users: `${ENV.APP.BASE_URL}/users{/id}`,
+    status: `${ENV.APP.BASE_URL}/status`
+  });
+});
 
 app.get('/status', (req, res) => {
   res.send({status: 'ok'});
