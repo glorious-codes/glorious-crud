a374a28eb4be8591132ae7275302368fe7e0eb8c
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-09 15:34:25 -0300

    Created base providers

---
+++ src/providers/base.js
@@ -0,0 +1,10 @@
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+
+module.exports = {
+  cors,
+  bodyParser,
+  app
+}
