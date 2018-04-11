ddacc5525c59c5e993aac1492794b3f1f341a80e
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-11 00:32:22 -0300

    Minor fix on coverage script file

--- .circleci/coverage.sh
+++ .circleci/coverage.sh
@@ -3,7 +3,7 @@
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
 if [ "$branch" = "master" ]; then
-    npm install -g codecov && codecov
+    npm install -g codecov --unsafe-perm && codecov
 else
     echo "skipped"
 fi
