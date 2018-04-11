7380fa8eb8dd0fee0566970ec3bef167c27f4419
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-11 00:46:49 -0300

    Removing unnecessary file

--- .circleci/coverage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-
-if [ "$branch" = "master" ]; then
-    npm install codecov && ./node_modules/codecov/bin/codecov
-else
-    echo "skipped"
-fi
