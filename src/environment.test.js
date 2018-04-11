211786fc6f096cb987e0edd5ba8ec7c060b5dfcc
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-10 21:26:36 -0300

    Moving db production info to env vars

--- src/environment.test.js
+++ src/environment.test.js
@@ -8,8 +8,11 @@ describe('Environment', () => {
 
   it('should export production environment if production env argument is given', () => {
     process.env.NODE_ENV = 'production';
+    process.env.DB_USER = 'rafael';
+    process.env.DB_PASS = '123';
     const PROD = require('../environments/production.json');
     const ENV = require('./environment')();
+    PROD.DB.BASE_URL = PROD.DB.BASE_URL.replace('<USER>', 'rafael').replace('<PASS>', '123');
     expect(ENV).toEqual(PROD);
   });
 });
