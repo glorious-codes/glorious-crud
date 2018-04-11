11a4358d865d0f08c70a16374272af2d669ffd72
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-11 00:01:52 -0300

    Removing unnecessary code

--- src/environment.js
+++ src/environment.js
@@ -5,9 +5,7 @@ const DEV = require('../environments/development.json');
 module.exports = function(){
   if(process.env.NODE_ENV == 'production'){
     PROD.DB.BASE_URL = PROD.DB.BASE_URL.replace('<USER>', argv.dbuser).replace('<PASS>', argv.dbpass);
-    console.log(PROD);
     return PROD;
   }
-  console.log(DEV);
   return DEV;
 };
