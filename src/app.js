bf0c1172669389a3c1d44325e6af5bfed124ba80
Rafael Camargo <contato@rafaelcamargo.com>
2018-04-30 23:11:12 -0300

    Getting app initialization straightforward

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
 const express = require('express');
 const GCrud = require('./');
-const envProd = require('../environments/prod.json');
-const envDev = require('../environments/dev.json');
-const env = process.env.NODE_ENV == 'production' ? envProd : envDev;
+const env = require('../environments/dev.json');
 
 const app = express();
 const gCrud = new GCrud(env.db.url, env.db.name, app);
 
-app.use(cors());
-app.use(bodyParser.json());
-
 const usersResource = gCrud.build('users');
+
+usersResource.get('/users/:id/custom', (req, res) => {
+  res.send({message: 'My custom endpoint'});
+});
 
 const beersResource = gCrud.build('beers', {
   onPostSuccess: (req, res, result) => {
-    res.send({
+    res.status(201).send({
       message: 'Beer successfully saved!',
-      user: result
+      id: result.body._id
     });
   }
 });
